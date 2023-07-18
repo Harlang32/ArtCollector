e50bb45d7b78785042cc3dd90fb48e89320119c8
Harlan George <hgeorge29@gmail.com>
2023-07-17 20:02:09 -0400

    finished Feature

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -29,9 +29,28 @@ import { fetchQueryResultsFromTermAndValue } from '../api';
  * finally:
  *  - call setIsLoading, set it to false
  */
-const Searchable = (props) => {
+const Searchable = ({searchTerm, searchValue, setIsLoading, setSearchResults}) => {
+  return 
+    <span className="content">
+      <a href="#" onClick={async (event) => {
+        event.preventDefault();
+       setIsLoading(true)
+       try {
+        const results =  await fetchQueryResultsFromTermAndValue(searchTerm, searchValue)
+        setSearchResults(results)
+        
+       } catch (error) {
+         console.error(error)
+       } finally {
+        setIsLoading(false);
+       }
   
-}
+      }}>
+        {searchValue}
+      </a>
+    </span>
+  
+};
 
 /**
  * We need a new component called Feature which looks like this when no featuredResult is passed in as a prop:
@@ -67,8 +86,155 @@ const Searchable = (props) => {
  * 
  * This component should be exported as default.
  */
-const Feature = (props) => {
+const Feature = ({ featuredResult, setSearchResults, setIsLoading }) => {
+  if (!featuredResult) {
+    return <main id="feature"></main>;
+  }
+  return (
+    <main id="feature">
+      <div className="object-feature">
+        <header>
+          <h3>{featuredResult.title}</h3>
+          <h4>{featuredResult.dated}</h4>
+        </header>
+        <section>
+          {featuredResult.description ? (
+            <section className="facts">
+              <span className="title">Description</span>
+              <span className="content">{featuredResult.description}</span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.style ? (
+            <section className="facts">
+              <span className="title">Style</span>
+              <span className="content">{featuredResult.style}</span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.dimensions ? (
+            <section className="facts">
+              <span className="title">Dimensions</span>
+              <span className="content">{featuredResult.dimensions}</span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.department ? (
+            <section className="facts">
+              <span className="title">Department</span>
+              <span className="content">{featuredResult.department}</span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.division ? (
+            <section className="facts">
+              <span className="title">Division</span>
+              <span className="content">{featuredResult.division}</span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.contact ? (
+            <section className="facts">
+              <span className="title">Contact</span>
+              <span className="content">{featuredResult.contact}</span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.creditline ? (
+            <section className="facts">
+              <span className="title">Creditline</span>
+              <span className="content">{featuredResult.creditline}</span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.culture ? (
+            <section className="facts">
+              <span className="title">Culture</span>
+              <span className="content">
+                <Searchable
+                  searchTerm="culture"
+                  searchValue={featuredResult.culture}
+                  setIsLoading={setIsLoading}
+                  setSearchResults={setSearchResults}
+                />
+              </span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.technique ? (
+            <section className="facts">
+              <span className="title">Technique</span>
+              <span className="content">
+                <Searchable
+                  searchTerm="technique"
+                  searchValue={featuredResult.technique}
+                  setIsLoading={setIsLoading}
+                  setSearchResults={setSearchResults}
+                />
+              </span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.medium ? (
+            <section className="facts">
+              <span className="title">Medium</span>
+              <span className="content">
+                <Searchable
+                  searchTerm="medium"
+                  searchValue={featuredResult.medium}
+                  setIsLoading={setIsLoading}
+                  setSearchResults={setSearchResults}
+                />
+              </span>
+            </section>
+          ) : null}
+        </section>
+        <section>
+          {featuredResult.people ? (
+            <section className="facts">
+              <span className="title">Person</span>
+              <span className="content">
+                {featuredResult.people.map((person) => {
+                  return (
+                    <Searchable
+                      searchTerm="person"
+                      searchValue={person.displayname}
+                      setIsLoading={setIsLoading}
+                      setSearchResults={setSearchResults}
+                    />
+                  );
+                })}
+              </span>
+            </section>
+          ) : null}
+        </section>
 
-}
+        <section className="photos">
+          {featuredResult.images.map((primaryimageurl, index) => {
+            return (
+              <span key={`${primaryimageurl} ${index}`}>
+                {featuredResult.primaryimageurl ? (
+                  <img
+                    src={featuredResult.primaryimageurl}
+                    alt={featuredResult.images}
+                  />
+                ) : null}
+              </span>
+            );
+          })}{" "}
+        </section>
+      </div>
+    </main>
+  );
+};
 
 export default Feature;
